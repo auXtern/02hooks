abf7bb2456e758379d15222ae2451461d342c42c
Abdullah Ubaid <Abdullahfak.au@gmail.com>
2022-07-10 17:32:33 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Post from "./post";
 
-function App() {
+const App = ({initialCount}) => {
+  const [state, setState] = useState({
+    count: initialCount,
+    user: "Abdullah Ubaid",
+  });
+
+  let [posts, setPosts] = useState([
+    {
+      name: 'Super awesome post',
+      body: 'The content of the post'
+    },
+    {
+      name: 'JS is great',
+      body: 'Something else'
+    }
+  ])
+
+  const restOne = () => {
+    setState({
+      ...state,
+      count: state.count-1
+    })
+  }
+  
+  const addOnePost = () => {
+    let newPost = {
+      name: 'PHP is still awesome',
+      body: 'Something else'
+    }
+
+    setPosts([
+      ...posts,
+      newPost
+    ])
+  }
+
+  const removePost = () => {
+    setPosts([])
+  }
+
+  useEffect(()=>{
+    // console.log('change on state')
+  }, [state]);
+
+  useEffect(()=>{
+    // console.log('change on posts')
+  }, [posts]);
+
+  useEffect(()=>{
+    // console.log('Mounted')
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1>{state.user}</h1>
+      <h3>Count: {state.count}</h3>
+      <button onClick={()=>setState({
+        ...state,
+        count:state.count + 1
+        })}> Add One +1 </button>
+      <button onClick={restOne}> Add One -1 </button>
+      <button onClick={()=>setState({
+        ...state,
+        count: initialCount
+      })}> Reset </button>
+
+        <hr/>
+        { posts.map((item, i) => (
+          <Post key={i} item={item}/>
+        )) }
+
+        <button
+          onClick={addOnePost}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add one more
+        </button>
+        <button
+          onClick={removePost}
+        >
+          Remove Post
+        </button>
+    </>
   );
 }
 
